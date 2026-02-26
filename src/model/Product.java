0b63549a015af30fe3534b6ca7a7993165265c68
shashwat-009 <shashwat.pratapsingh04@gmail.com>
2026-02-26 17:01:57 +0530

    Worked on first draft of Product.java class

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,4 +1,68 @@
 package model;
 
 public class Product {
+    private int ProductId;
+    private String ProductName;
+    private double ProductPrice;
+    private int ProductQty;
+    private String ProductCategory;
+
+    public Product(int productId, String productName, double productPrice, int productQty, String productCategory) {
+        this.ProductId = productId;
+        this.ProductName = productName;
+        this.ProductPrice = productPrice;
+        this.ProductQty = productQty;
+        this.ProductCategory = productCategory;
+    }
+
+    public int getProductId() {
+        return ProductId;
+    }
+
+    public void setProductId(int productId) {
+        ProductId = productId;
+    }
+
+    public String getProductName() {
+        return ProductName;
+    }
+
+    public void setProductName(String productName) {
+        ProductName = productName;
+    }
+
+    public double getProductPrice() {
+        return ProductPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        ProductPrice = productPrice;
+    }
+
+    public int getProductQty() {
+        return ProductQty;
+    }
+
+    public void setProductQty(int productQty) {
+        ProductQty = productQty;
+    }
+
+    public String getProductCategory() {
+        return ProductCategory;
+    }
+
+    public void setProductCategory(String productCategory) {
+        ProductCategory = productCategory;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "ProductId=" + ProductId +
+                ", ProductName='" + ProductName + '\'' +
+                ", ProductPrice=" + ProductPrice +
+                ", ProductQty=" + ProductQty +
+                ", ProductCategory='" + ProductCategory + '\'' +
+                '}';
+    }
 }
